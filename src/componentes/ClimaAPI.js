f02a38cf3cc99fb28e868cc8bf7418a80cd35cef
Rodrigo Sánchez <rodri-23-11@hotmail.com>
2023-08-22 16:53:05 -0300

    Trabajando con API

--- src/componentes/ClimaAPI.js
+++ src/componentes/ClimaAPI.js
@@ -1,31 +1,86 @@
 import React, { useDebugValue } from "react";
+import './ClimaAPI.css';
 
 export default class ClimaAPI extends React.Component{
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
-            climas: []
-        }
+            climasCor: [],
+            climasBsAS: []
+        };
 
     }
 
     componentDidMount() {
-        fetch("https://api.openweathermap.org/data/2.5/weather?q=Cordoba&APPID=4a52d040f7ab75447dd871c1c1da5aba&units=metric")
-        .then(res => res.json() )
-        .then( (result) => {
-            console.log(result)
-            this.setState({
-                climas: result.temp
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=-31.4135&lon=-64.18105&appid=4a52d040f7ab75447dd871c1c1da5aba&units=metric")
+           .then(res => res.json() )
+           .then(result => {
+               console.log(result);
+               this.setState({
+                  climasCor: [result]
             });
-        })
+        });
+        fetch("https://api.openweathermap.org/data/2.5/weather?lat=-34.61315&lon=-58.37723&appid=4a52d040f7ab75447dd871c1c1da5aba&units=metric")
+           .then(res => res.json() )
+           .then(result => {
+               console.log(result);
+               this.setState({
+                  climasBsAS: [result]
+            });
+        });
     }
     render(){
         return (
             <>
-            {this.state.climas.map((clima) => (
-                <span>Climas</span>
-            ))}
+            <div>
+                {this.state.climasCor.map((clima, index) => (
+                    <div>
+                        <div className="ciudad">
+                            <h2 key={index}>{clima.name}</h2>
+                        </div>
+                        <div className="dato">
+                            <h5>Temperatura máxima</h5>
+                            <h5 key={index}>{clima.main.temp_max}°C</h5>
+                        </div>
+                        <div className="dato">
+                            <h5>Temperatura mínima</h5>
+                        <h5 key={index}>{clima.main.temp_min}°C</h5>
+                        </div>
+                        <div className="dato">
+                            <h5>Humedad</h5>
+                        <h5 key={index}>{clima.main.humidity}</h5>
+                        </div>
+                        <div className="dato">
+                        <h5>Viento</h5>
+                        <h5 key={index}>{clima.wind.speed}m/s</h5>
+                        </div>
+                    </div>
+                ))}
+                {this.state.climasBsAS.map((clima, index) => (
+                    <div>
+                        <div className="ciudad">
+                            <h2 key={index}>{clima.name}</h2>
+                        </div>
+                        <div className="dato">
+                            <h5>Temperatura máxima</h5>
+                            <h5 key={index}>{clima.main.temp_max}°C</h5>
+                        </div>
+                        <div className="dato">
+                            <h5>Temperatura mínima</h5>
+                        <h5 key={index}>{clima.main.temp_min}°C</h5>
+                        </div>
+                        <div className="dato">
+                            <h5>Humedad</h5>
+                        <h5 key={index}>{clima.main.humidity}</h5>
+                        </div>
+                        <div className="dato">
+                        <h5>Viento</h5>
+                        <h5 key={index}>{clima.wind.speed}m/s</h5>
+                        </div>
+                    </div>
+                ))}
+            </div>
             </>
 
         )
--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -13,24 +13,24 @@ export default class Inicio extends React.Component {
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
                             <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 4"></button>
-                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 5"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+                            <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="4" aria-label="Slide 5"></button>
                         </div>
                         <div class="carousel-inner">
                             <div class="carousel-item active">
-                             <img src="../recursos/inicio1.jpg" class="d-block w-100" alt="..."/>
+                             <img src="../recursos/inicio1.jpg" class="d-block w-100 imagenesInicio" alt="..."/>
                             </div>
                             <div class="carousel-item">
-                             <img src="../recursos/inicio2.jpg" class="d-block w-100" alt="..."/>
+                             <img src="../recursos/inicio2.jpg" class="d-block w-100 imagenesInicio" alt="..."/>
                             </div>
                             <div class="carousel-item">
-                              <img src="../recursos/inicio3.jpg" class="d-block w-100" alt="..."/>
+                              <img src="../recursos/inicio3.jpg" class="d-block w-100 imagenesInicio" alt="..."/>
                             </div>
                             <div class="carousel-item">
-                              <img src="../recursos/inicio4.jpg" class="d-block w-100" alt="..."/>
+                              <img src="../recursos/inicio4.jpg" class="d-block w-100 imagenesInicio" alt="..."/>
                             </div>
                             <div class="carousel-item">
-                              <img src="../recursos/inicio5.jpg" class="d-block w-100" alt="..."/>
+                              <img src="../recursos/inicio5.png" class="d-block w-100 imagenesInicio" alt="..."/>
                             </div>
                         </div>
                         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
@@ -43,7 +43,9 @@ export default class Inicio extends React.Component {
                         </button>
                     </div>
                 </div>
-                <aside><ClimaAPI /></aside>
+                <aside>
+                    <ClimaAPI/>
+                </aside>
             </div>
         )
     }
